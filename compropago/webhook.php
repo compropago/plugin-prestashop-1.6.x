24c529c06b62248f1cee4eb83c085265a38f12de
Rolando Lucio <rolandolucio@gmail.com>
2016-01-06 19:33:10 -0600

    wh

--- compropago/webhook.php
+++ compropago/webhook.php
@@ -15,28 +15,56 @@
 * limitations under the License.
 */
 /**
- * Compropago WebHook
+ * ComproPago Prestashop WebHook
  * @author Rolando Lucio <rolando@compropago.com>
  */
-
-
-//include prestashop
-
-
-
-include_once	dirname(__FILE__).'/../../config/config.inc.php';
-include_once	dirname(__FILE__).'/../../init.php';
-include_once	dirname(__FILE__).'/../../classes/PrestaShopLogger.php';
-
+//El request es valido?
+$request = @file_get_contents('php://input');
+if(!$jsonObj = json_decode($request)){
+	die('Tipo de Request no Valido');
+}
+//Include prestashop files
+$prestaFiles= array(
+		dirname(__FILE__).'/../../config/config.inc.php',
+		dirname(__FILE__).'/../../init.php',
+		dirname(__FILE__).'/../../classes/PrestaShopLogger.php'
+);
+foreach($prestaFiles as $prestaFile){
+	if(file_exists($prestaFile)){
+		include_once	$prestaFile;
+	}else{
+		echo "ComproPago Warning: No se encontro el archivo de Prestashop:".$prestaFile."<br>";
+	}
+}
 //include ComproPago SDK & dependecies
-$compropagoComposer= 'vendor/autoload.php';
+$compropagoComposer= dirname(__FILE__).'/vendor/autoload.php';
 if ( file_exists( $compropagoComposer ) ){
 	require $compropagoComposer;
 }else{
 	die('No se encontro el autoload para Compropago y sus dependencias:'.$compropagoComposer);
 }
+//Compropago Plugin Installed?
+if (!Module::isInstalled('compropago')){
+	die('El módulo de ComproPago no se encuentra instalado');
+}
+//ComproPago Config Keys
+$config = Configuration::getMultiple(array('COMPROPAGO_PUBLICKEY', 'COMPROPAGO_PRIVATEKEY'));
+if (!isset($config['COMPROPAGO_PUBLICKEY']) || !isset($config['COMPROPAGO_PRIVATEKEY'])
+	|| empty($config['COMPROPAGO_PUBLICKEY']) || empty($config['COMPROPAGO_PRIVATEKEY'])){
+	die("Se requieren las llaves de compropago");
+}
+$compropagoConfig= array(
+		'publickey'=>$config['COMPROPAGO_PUBLICKEY'],
+		'privatekey'=>$config['COMPROPAGO_PRIVATEKEY']
+		//'live'=>false
+);
+
+$compropagoClient= new Compropago\Client($compropagoConfig);
+$compropagoService= new Compropago\Service($compropagoClient);
+//Val keys vs mode here
+
+$response=Compropago\Http\Rest::doExecute($compropagoClient,'users/auth/',$data);
+//Ill check my self i dont belive
 
 
-$body = @file_get_contents('php://input');
-
-print_r($body);
+var_dump($response);
