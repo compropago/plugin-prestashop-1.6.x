afdf02d13bf51d846d71acaf48bea8c174d0e7a6
Jesus Alfredo <alfredo@compropago.com>
2017-09-12 20:20:47 -0500

    replace sdk to new version in webhooks

--- webhook.php
+++ webhook.php
@@ -43,7 +43,7 @@ require_once __DIR__.'/../../classes/order/OrderHistory.php';
      ]);
  }
  
- 
+ $config        = Configuration::getMultiple(array('COMPROPAGO_PUBLICKEY', 'COMPROPAGO_PRIVATEKEY','COMPROPAGO_MODE'));
  $publickey     = $config['COMPROPAGO_PUBLICKEY'];
  $privatekey    = $config['COMPROPAGO_PRIVATEKEY'];
  $live          = ($config['COMPROPAGO_MODE']==true);
@@ -59,34 +59,89 @@ require_once __DIR__.'/../../classes/order/OrderHistory.php';
            "reference" => null
          ]);
      }
- 
-     $response = $client->api->verifyOrder($resp_webhook->id);
 
-     switch ($response->type){
-         case 'charge.success':
-             // TODO: Actions on success payment
-             break;
-         case 'charge.pending':
-             // TODO: Actions on pending payment
-             break;
-         case 'charge.expired':
-             // TODO: Actions on expired payment
-             break;
-         default:
-             echo json_encode([
-               "status" => "error",
-               "message" => "invalid request type",
-               "short_id" => $response->short_id,
-               "reference" => null
-             ]);
-     }
- 
-     echo json_encode([
-       "status" => "success",
-       "message" => "OK",
-       "short_id" => $response->short_id,
-       "reference" => 'internal-1234'
-     ]);
+    /**
+     * Validate transaction tables
+     */
+    if(
+        !Db::getInstance()->execute("SHOW TABLES LIKE '"._DB_PREFIX_ ."compropago_orders'") ||
+        !Db::getInstance()->execute("SHOW TABLES LIKE '"._DB_PREFIX_ ."compropago_transactions'")
+    ){
+        die('ComproPago Tables Not Found');
+    }
+
+    $sql = "SELECT * FROM "._DB_PREFIX_."compropago_orders	WHERE compropagoId = '".$resp_webhook->id."' ";
+
+    if ($row = Db::getInstance()->getRow($sql)){
+        /**
+         * Generamos las rutinas correspondientes para cada uno de los casos posible del webhook
+         */
+        $response = $client->api->verifyOrder($resp_webhook->id);
+        
+        switch ($response->type){
+            case 'charge.success':
+                
+                $nomestatus = "COMPROPAGO_SUCCESS";
+                break;
+            case 'charge.pending':
+                $nomestatus = "COMPROPAGO_PENDING";
+                break;
+            case 'charge.expired':
+                $nomestatus = "COMPROPAGO_EXPIRED";
+                break;
+            default:
+                echo json_encode([
+                "status" => "error",
+                "message" => "invalid request type",
+                "short_id" => $response->short_id,
+                "reference" => null
+                ]);
+        }
+
+        /**
+         * Cambio de estatus para las ordenes
+         */
+        $id_order   = intval($response->order_info->order_id);
+        $recordTime = time();
+
+        $order   = new Order($id_order);
+        $history = new OrderHistory();
+
+        $history->id_order = (int)$order->id;
+        $history->changeIdOrderState((int)Configuration::get($nomestatus), (int)($order->id));
+
+        $history->addWithemail();
+        $history->save();
+
+        /**
+         * Actualizacion de base de datos
+         */
+        $prefix = _DB_PREFIX_;
+        $sql = "UPDATE `{$prefix}compropago_orders` SET `modified` = '$recordTime', `compropagoStatus` = '{$response->type}', `storeExtra` = '$nomestatus' WHERE `id` = '{$response->id}'";
+
+        if(!Db::getInstance()->execute($sql)){
+            die("Error Updating ComproPago Order Record at Store");
+        }
+
+        $ioIn  = base64_encode(serialize($resp_webhook));
+        $ioOut = base64_encode(serialize($response));
+
+        Db::getInstance()->autoExecute(_DB_PREFIX_ . 'compropago_transactions', array(
+            'orderId' 			   => $row['id'],
+            'date' 				   => $recordTime,
+            'compropagoId'		   => $response->id,
+            'compropagoStatus'	   => $response->type,
+            'compropagoStatusLast' => $row['compropagoStatus'],
+            'ioIn' 				   => $ioIn,
+            'ioOut' 			   => $ioOut
+        ),'INSERT');
+
+        echo('Orden '.$resp_webhook->id.' Confirmada');
+
+    }else{
+        die('El número de orden no se encontro en la tienda');
+    }
+
  }catch (Exception $e) {
      echo json_encode([
        "status" => "error",
